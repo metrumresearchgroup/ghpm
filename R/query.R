4f351296fb1bf93591ad3d11ec55c0dba40d246a
Harsh Baid <harshb@metrumrg.com>
2019-04-22 15:30:55 -0400

    feat: added get_milestones()

--- R/query.R
+++ R/query.R
@@ -5,8 +5,28 @@
 #' @return The list containing the query result
 #' @importFrom gh gh
 #' @importFrom purrr compact
-graphql_query <- function(file, ..., .api_endpoint = "https://api.github.com/graphql") {
+#' @export
+graphql_query <- function(file, ..., .api_url = "https://api.github.com/graphql") {
 	file <- system.file(file, package = "ghpm")
 	query <- readChar(file, file.info(file)$size)
-	return(gh("POST /graphql", query = query, variables = compact(list(...)), .api_url = .api_url))
+	return(gh("POST ", query = query, variables = compact(list(...)), .api_url = .api_url)$data)
+}
+
+#' Gets a data frame of the milestones associated with a given repo.
+#' @param org Name of organization to query
+#' @param repo Name of repository to query
+#' @param .api_url Optional API url to query. Defaults to "https://api.github.com/graphql"
+#' @return A data frame containing the Title | Description | State | Author | Url of each milestone
+#' @export
+get_milestones <- function(org, repo, .api_url = "https://api.github.com/graphql"){
+	data <- graphql_query("milestones.graphql", org = org, repo = repo, .api_url = .api_url)$repository$milestones$nodes
+	output <- reduce(data, function(.acc, .cv){
+		return(.acc %>% add_row("Title" = .cv$title,
+								"Description" = .cv$description,
+								"State" = .cv$state,
+								"Author" = .cv$creator$login,
+								"Url" = .cv$url))
+	}, .init = tibble("Title" = NA, "Description" = NA, "State" = NA, "Author" = NA, "Url" = NA))
+
+	return(output)
 }
