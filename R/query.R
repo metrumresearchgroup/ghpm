c86a17dfc79a466f806319ddabd5f4a3aa484a44
Harsh Baid <harshb@metrumrg.com>
2019-04-22 20:01:42 -0400

    feat: added get_issues()

--- R/query.R
+++ R/query.R
@@ -17,16 +17,33 @@ graphql_query <- function(file, ..., .api_url = "https://api.github.com/graphql"
 #' @param repo Name of repository to query
 #' @param .api_url Optional API url to query. Defaults to "https://api.github.com/graphql"
 #' @return A data frame containing the Title | Description | State | Author | Url of each milestone
+#' @importFrom purrr reduce
+#' @importFrom tibble tibble add_row
 #' @export
 get_milestones <- function(org, repo, .api_url = "https://api.github.com/graphql"){
 	data <- graphql_query("milestones.graphql", org = org, repo = repo, .api_url = .api_url)$repository$milestones$nodes
-	output <- reduce(data, function(.acc, .cv){
+	milestones <- reduce(data, function(.acc, .cv){
 		return(.acc %>% add_row("Title" = .cv$title,
 								"Description" = .cv$description,
 								"State" = .cv$state,
 								"Author" = .cv$creator$login,
 								"Url" = .cv$url))
 	}, .init = tibble("Title" = NA, "Description" = NA, "State" = NA, "Author" = NA, "Url" = NA))
+	return(milestones)
+}
 
-	return(output)
+#' Gets a data frame of the issues associated with a given repo
+#' @inheritParams get_milestones
+#' @return A data frame containing the Title | Body | Author | Number | Labels | Milestone | State of each issue
+#' @importFrom purrr reduce
+#' @importFrom tibble tibble add_row
+get_issues <- function(org, repo, .api_url = "https://api.github.com/graphql"){
+	data <- graphql_query("issues.graphql", org = "metrumresearchgroup", repo = "pkgr")$repository$issues$nodes
+	issues <- reduce(data, function(.acc, .cv){
+		.acc <- .acc %>% add_row("Title" = .cv$title, "Body" = .cv$bodyText, "Author" = .cv$author$login, "Number" = .cv$number, "Labels" = ifelse(!is_empty(.cv$labels$nodes), .cv$labels$nodes[[1]]$name, NA), "Milestone" = ifelse(is.null(.cv$milestone), "None", .cv$milestone), "State" = .cv$state)
+
+		return(.acc)
+	}, .init = tibble("Title" = NA, "Body" = NA, "Author" = NA, "Number" = NA, "Labels" = NA, "Milestone" = NA, "State" = NA))
+
+	return(issues)
 }
