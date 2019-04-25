0effbe51b1ff4376bbb670a9d498b2cd1d10395a
Harsh Baid <harshb@metrumrg.com>
2019-04-25 14:28:09 -0400

    feat: added ability to pull issue events + added header functionality to graphql_query()

--- R/query.R
+++ R/query.R
@@ -2,14 +2,15 @@
 #' @param file Query file to execute
 #' @param ... Additional variables to pass on to the query
 #' @param .api_url Optional API url to query. Defaults to "https://api.github.com/graphql"
+#' @param .header Optional vector of headers to send to query
 #' @return The list containing the query result
 #' @importFrom gh gh
 #' @importFrom purrr compact
 #' @export
-graphql_query <- function(file, ..., .api_url = "https://api.github.com/graphql") {
+graphql_query <- function(file, ..., .api_url = "https://api.github.com/graphql", .header = NULL) {
 	file <- system.file(file, package = "ghpm")
 	query <- readChar(file, file.info(file)$size)
-	return(gh("POST ", query = query, variables = compact(list(...)), .api_url = .api_url)$data)
+	return(gh("POST ", query = query, variables = compact(list(...)), .api_url = .api_url, .send_headers = .header)$data)
 }
 
 #' Gets a data frame of the milestones associated with a given repo.
@@ -46,4 +47,25 @@ get_issues <- function(org, repo, .api_url = "https://api.github.com/graphql"){
 	}, .init = tibble("Title" = NA, "Body" = NA, "Author" = NA, "Number" = NA, "Labels" = NA, "Milestone" = NA, "State" = NA))
 
 	return(issues)
+}
+
+#' Gets a data frame of the project board events of each issue
+#' @inheritParams get_milestones
+#' @return A data frame containing Issue | Project | Type | Column | Author | Date of each issue
+#' @importFrom purrr reduce map_df keep
+#' @importFrom tibble tibble add_row
+#' @importFrom dplyr mutate arrange
+get_issue_events <- function(org, repo, .api_url = "https://api.github.com/graphql"){
+	data <- graphql_query("issue_events.graphql", org = "metrumresearchgroup", repo = "pkgr", .header = c("Accept" = "application/vnd.github.starfox-preview+json"))$repository$issues$nodes
+	data <- keep(data, ~length(.x$timelineItems$nodes) > 0)
+
+	timeline <- map_df(data, function(x){
+		event_data <- reduce(x$timelineItems$nodes, function(.acc, .cv){
+			return(.acc %>% add_row("Project" = .cv$project$name, "Type" = ifelse(.cv$`__typename` == "AddedToProjectEvent", "Added", "Moved"), "Column" = .cv$projectColumnName, "Author" = .cv$actor$login, "Date" = .cv$createdAt))
+		}, .init = tibble("Project" = character(), "Type" = character(), "Column" = character(), "Author" = character(), "Date" = character(), .rows = 0))
+
+		return(event_data %>% mutate("Issue" = x$number))
+	})
+
+	return(timeline %>% arrange(Project))
 }
