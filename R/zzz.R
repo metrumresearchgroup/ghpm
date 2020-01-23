aefccb5aa7d8261353903a932c71b3a650783cf3
harshb <harshb@metrumrg.com>
2020-01-23 10:48:28 -0500

    fix: default options onLoad were set improperly
    References: #49

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,8 @@
 .onLoad <- function(libname, pkgname) {
 	op <- options()
 	op.ghpm <- list(
-		ghpm.api_url = "https://api.github.com"
+		ghpm.scheme = "https",
+		ghpm.hostname = "api.github.com"
 	)
 	toset <- !(names(op.ghpm) %in% names(op))
 	if (any(toset)) options(op.ghpm[toset])
