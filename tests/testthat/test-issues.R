4be948c3457b40cf9b1b65de0d617099aba42784
harshb <harshb@metrumrg.com>
2020-01-24 16:45:09 -0500

    fix: fixed all broken tests

--- tests/testthat/test-issues.R
+++ tests/testthat/test-issues.R
@@ -1,73 +1,82 @@
 context("test-issues")
 
-test_that("can get repo issues", {
-	mockery::stub(
-		get_repo_issues,
-		"graphql_query",
-		jsonlite::read_json("issues/issues_response.json"),
-		depth = 2
-	)
-	result <- get_repo_issues("test", "test")
-	expect_equal(names(result), c("issue", "title", "body", "creator", "milestone", "state"))
-	expect_true(nrow(result) > 0)
-	expect_true(is.data.frame(result))
-})
-
 describe("issue functions", {
 
-	test_that("can get repo issue labels", {
-		mockery::stub(
-			get_repo_issue_labels,
-			"graphql_query",
-			jsonlite::read_json("issues/issue_labels_response.json")
+	test_that("it can get repo issues", {
+		result <- with_mock(
+			graphql_query = function(file, ..., .api_url, .header) {
+				return(jsonlite::read_json("issues/issues_response.json"))
+			},
+			get_repo_issues("test", "test")
 		)
-		result <- get_repo_issue_labels("test", "test")
+
+		expect_true(nrow(result) > 0)
+		expect_equal(names(result), c("issue", "title", "body", "creator", "milestone", "state"))
+		expect_true(is.data.frame(result))
+	})
+
+	test_that("it can get repo issue labels", {
+		result <- with_mock(
+			graphql_query = function(file, ..., .api_url, .header) {
+				return(jsonlite::read_json("issues/issue_labels_response.json"))
+			},
+			get_repo_issue_labels("test", "test")
+		)
+
 		expect_true(nrow(result) > 0)
 		expect_equal(names(result), c("issue", "label"))
 		expect_true((is.data.frame(result)))
 	})
 
-	# test_that("can get repo issue assignees", {
-	# 	mockery::stub(
-	# 		get_issue_assignees,
-	# 		"graphql_query",
-	# 		jsonlite::read_json("issues/issue_assignees_response.json")
-	# 	)
-	# 	result <- get_issue_assignees("test", "test")
-	# 	expect_equal(names(result), c("issue", "assigned_to"))
-	# 	expect_true((is.data.frame(result)))
-	# })
-	#
-	# test_that("can get issue events", {
-	# 	mockery::stub(
-	# 		get_issue_events,
-	# 		"graphql_query",
-	# 		jsonlite::read_json("issues/issue_events_response.json")
-	# 	)
-	# 	result <- get_issue_events("test", "test")
-	# 	expect_equal(names(result), c("issue", "project", "type", "column", "author", "date"))
-	# 	expect_true((is.data.frame(result)))
-	# })
-	#
-	# test_that("can get issue comments", {
-	# 	mockery::stub(
-	# 		get_issue_comments,
-	# 		"graphql_query",
-	# 		jsonlite::read_json("issues/issue_comments_response.json")
-	# 	)
-	# 	result <- get_issue_comments("test", "test")
-	# 	expect_equal(names(result), c("issue", "comment", "author", "date"))
-	# 	expect_true((is.data.frame(result)))
-	# })
-	#
-	# test_that("can get issues by a specific milestone", {
-	# 	mockery::stub(
-	# 		get_issues_from_milestone,
-	# 		"graphql_query",
-	# 		jsonlite::read_json("issues/issue_milestone_response.json")
-	# 	)
-	# 	result <- get_issues_from_milestone("test", "test", 5)
-	# 	expect_equal(names(result), c("issue", "title", "body", "creator", "state"))
-	# 	expect_true((is.data.frame(result)))
-	# })
+	test_that("it can get repo issue assignees", {
+		result <- with_mock(
+			graphql_query = function(file, ..., .api_url, .header) {
+				return(jsonlite::read_json("issues/issue_assignees_response.json"))
+			},
+			get_issue_assignees("test", "test")
+		)
+
+		expect_true(nrow(result) > 0)
+		expect_equal(names(result), c("issue", "assigned_to"))
+		expect_true((is.data.frame(result)))
+	})
+
+	test_that("it can get issue events", {
+		result <- with_mock(
+			graphql_query = function(file, ..., .api_url, .header) {
+				return(jsonlite::read_json("issues/issue_events_response.json"))
+			},
+			get_issue_events("test", "test")
+		)
+
+		expect_true(nrow(result) > 0)
+		expect_equal(names(result), c("issue", "project", "type", "column", "author", "date"))
+		expect_true((is.data.frame(result)))
+	})
+
+	test_that("it can get issue comments", {
+		result <- with_mock(
+			graphql_query = function(file, ..., .api_url, .header) {
+				return(jsonlite::read_json("issues/issue_comments_response.json"))
+			},
+			get_issue_comments("test", "test")
+		)
+
+		expect_true(nrow(result) > 0)
+		expect_equal(names(result), c("issue", "comment", "author", "date"))
+		expect_true((is.data.frame(result)))
+	})
+
+	test_that("it can get issues by a specific milestone", {
+		result <- with_mock(
+			graphql_query = function(file, ..., .api_url, .header) {
+				return(jsonlite::read_json("issues/issue_milestone_response.json"))
+			},
+			get_issues_from_milestone("test", "test", 5)
+		)
+
+		expect_true(nrow(result) > 0)
+		expect_equal(names(result), c("issue", "title", "body", "creator", "state"))
+		expect_true((is.data.frame(result)))
+	})
 })
--- tests/testthat/test-pullrequests.R
+++ tests/testthat/test-pullrequests.R
@@ -1,48 +1,56 @@
-# context("test-pull-requests")
-#
-# describe("pull request functions", {
-#
-# 	it("can get pull request commits", {
-# 		mockery::stub(
-# 			get_pull_request_commits,
-# 			"graphql_query",
-# 			jsonlite::read_json("pullrequests/pull_request_commits_response.json")
-# 		)
-# 		result <- get_pull_request_commits("test", "test")
-# 		expect_equal(names(result), c("oid", "message", "author", "date"))
-# 		expect_true(is.data.frame(result))
-# 	})
-#
-# 	it("can get all pull requests", {
-# 		mockery::stub(
-# 			get_all_pull_requests,
-# 			"graphql_query",
-# 			jsonlite::read_json("pullrequests/all_pull_requests_response.json")
-# 		)
-# 		result <- get_all_pull_requests("test", "test")
-# 		expect_equal(names(result), c("pullrequest", "title", "author", "body", "milestone", "created_at", "merged_by", "merged_at", "merged_to", "state"))
-# 		expect_true(is.data.frame(result))
-# 	})
-#
-# 	it("can get all pull request reviewers", {
-# 		mockery::stub(
-# 			get_pull_request_reviewers,
-# 			"graphql_query",
-# 			jsonlite::read_json("pullrequests/pull_request_reviewers_response.json")
-# 		)
-# 		result <- get_pull_request_reviewers("test", "test")
-# 		expect_equal(names(result), c("pullrequest", "reviewer"))
-# 		expect_true(is.data.frame(result))
-# 	})
-#
-# 	it("can get all pull request comments", {
-# 		mockery::stub(
-# 			get_pull_request_comments,
-# 			"graphql_query",
-# 			jsonlite::read_json("pullrequests/pull_request_comments_response.json")
-# 		)
-# 		result <- get_pull_request_comments("test", "test", 111)
-# 		expect_equal(names(result), c("pullrequest", "author", "body", "created_at"))
-# 		expect_true(is.data.frame(result))
-# 	})
-# })
+context("test-pull-requests")
+
+describe("pull request functions", {
+
+	test_that("it can get pull request commits", {
+		result <- with_mock(
+			graphql_query = function(file, ..., .api_url, .header) {
+				return(jsonlite::read_json("pullrequests/pull_request_commits_response.json"))
+			},
+			get_pull_request_commits("test", "test")
+		)
+
+		expect_true(nrow(result) > 0)
+		expect_equal(names(result), c("oid", "message", "author", "date"))
+		expect_true(is.data.frame(result))
+	})
+
+	test_that("it can get all pull requests", {
+		result <- with_mock(
+			graphql_query = function(file, ..., .api_url, .header) {
+				return(jsonlite::read_json("pullrequests/all_pull_requests_response.json"))
+			},
+			get_all_pull_requests("test", "test")
+		)
+
+		expect_true(nrow(result) > 0)
+		expect_equal(names(result), c("pullrequest", "title", "author", "body", "milestone", "created_at", "merged_by", "merged_at", "merged_to", "state"))
+		expect_true(is.data.frame(result))
+	})
+
+	test_that("it can get all pull request reviewers", {
+		result <- with_mock(
+			graphql_query = function(file, ..., .api_url, .header) {
+				return(jsonlite::read_json("pullrequests/pull_request_reviewers_response.json"))
+			},
+			get_pull_request_reviewers("test", "test")
+		)
+
+		expect_true(nrow(result) > 0)
+		expect_equal(names(result), c("pullrequest", "reviewer"))
+		expect_true(is.data.frame(result))
+	})
+
+	test_that("it can get all pull request comments", {
+		result <- with_mock(
+			graphql_query = function(file, ..., .api_url, .header) {
+				return(jsonlite::read_json("pullrequests/pull_request_comments_response.json"))
+			},
+			get_pull_request_comments("test", "test", 111)
+		)
+
+		expect_true(nrow(result) > 0)
+		expect_equal(names(result), c("pullrequest", "author", "body", "created_at"))
+		expect_true(is.data.frame(result))
+	})
+})
